2651537686b3405e1b21c8199a17b243dc6e27f4
Jack Ziesing <jack.ziesing@jziesing-ltm5.internal.salesforce.com>
2020-02-06 01:50:18 -0800

    eon

--- worker.js
+++ worker.js
@@ -5,7 +5,8 @@
 
 
 let AWS = require("aws-sdk"),
-    Kafka = require('no-kafka');
+    Kafka = require('no-kafka'),
+    csv = require('csvtojson');;
 
 var s3  = new AWS.S3({
   accessKeyId: process.env.HDRIVE_S3_ACCESS_KEY,
@@ -21,15 +22,52 @@ var consumer = new Kafka.SimpleConsumer({
     }
 });
 
+var producer = new Kafka.Producer({
+    connectionString: process.env.KAFKA_URL,
+    ssl: {
+        cert: process.env.KAFKA_CLIENT_CERT,
+        key: process.env.KAFKA_CLIENT_CERT_KEY
+    }
+});
+
+
+async function csvToJSON(keyURL) {
+    // get csv file and create stream
+    console.log('keyURL');
+    console.log(keyURL);
+    const stream = s3.getObject({Bucket: 'quiz-playground', Key: keyURL}).createReadStream();
+    // convert csv file (stream) to JSON format data
+    const json = await csv().fromStream(stream);
+    var msgs =  [];
+    var newjson = json.map((data, index) => {
+        return {
+            topic: 'licking-49744.new_question',
+            partition: 0,
+            message: {
+                value: JSON.stringify(data)
+            }
+        }
+    });
+    console.log(newjson);
+    producer.send(newjson);
+    producer.init().then(function() {
+                        producer.send(newjson[0]);
+                    }).then(function(result) {
+                        console.log('kafka result');
+                        console.log(result);
+                    });
+};
+
 
 // data handler function can return a Promise
-var dataHandler = function (messageSet, topic, partition) {
-    messageSet.forEach(function (m) {
+var dataHandler = (messageSet, topic, partition) => {
+    messageSet.forEach((m) => {
         console.log(topic, partition, m.offset, m.message.value.toString('utf8'));
+        csvToJSON(m.message.value.toString('utf8'));
     });
 };
 
-return consumer.init().then(function () {
+return consumer.init().then(() => {
     // Subscribe partitons 0 and 1 in a topic:
-    return consumer.subscribe('licking-49744.add_qs_ms', [0, 1], dataHandler);
+    return consumer.subscribe('licking-49744.add_qs_ms', [0], dataHandler);
 });

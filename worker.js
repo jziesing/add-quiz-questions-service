d01880d004616c5d7e261daeaf6a8dda6caf2cd5
Jack Ziesing <jack.ziesing@jziesing-ltm5.internal.salesforce.com>
2020-02-06 18:04:02 -0800

    format msgs for kafka connect

--- worker.js
+++ worker.js
@@ -31,8 +31,11 @@ var producer = new Kafka.Producer({
 });
 
 
-async function fetchFileProduceKafkaMsgs(keyURL) {
+async function fetchFileProduceKafkaMsgs(msgData) {
     // get csv file and create stream
+    let newMsgData = JSON.parse(msgData);
+    let keyURL = newMsgData.file_path;
+    let quiz_sfid = newMsgData.quiz_sfid;
     console.log('keyURL');
     console.log(keyURL);
     const stream = s3.getObject({Bucket: 'quiz-playground', Key: keyURL}).createReadStream();
@@ -42,12 +45,78 @@ async function fetchFileProduceKafkaMsgs(keyURL) {
     var newjson = json.map((data, index) => {
         console.log('data');
         console.log(data);
-        console.log(JSON.stringify(data));
+        let formattedQuestionData = {
+                            "schema": {
+                                "type": "record",
+                                "fields": [
+                                    {
+                                        "type": "string",
+                                        "optional": false,
+                                        "field": "question__c"
+                                    },
+                                    {
+                                        "type": "string",
+                                        "optional": false,
+                                        "field": "quiz__c"
+                                    },
+                                    {
+                                        "type": "int64",
+                                        "optional": false,
+                                        "field": "correct_answer__c"
+                                    },
+                                    {
+                                        "type": "string",
+                                        "optional": false,
+                                        "field": "answer_1__c"
+                                    },
+                                    {
+                                        "type": "string",
+                                        "optional": false,
+                                        "field": "answer_2__c"
+                                    },
+                                    {
+                                        "type": "string",
+                                        "optional": false,
+                                        "field": "answer_3__c"
+                                    },
+                                    {
+                                        "type": "string",
+                                        "optional": false,
+                                        "field": "answer_4__c"
+                                    },
+                                    {
+                                        "type": "string",
+                                        "optional": false,
+                                        "field": "answer_5__c"
+                                    },
+                                    {
+                                        "type": "string",
+                                        "optional": false,
+                                        "field": "answer_6__c"
+                                    }
+                                ],
+                                "optional": false,
+                                "name": "ksql.users"
+                            },
+                            "payload": {
+                                "question__c": data.question,
+                                "quiz__c": quiz_sfid,
+                                "correct_answer__c": data.correct_answer,
+                                "answer_1__c": data.answer1,
+                                "answer_2__c": data.answer2,
+                                "answer_3__c": data.answer3,
+                                "answer_4__c": data.answer4,
+                                "answer_5__c": data.answer5,
+                                "answer_6__c": data.answer6
+                            }
+                        };
+
+        // console.log(JSON.stringify(formattedQuestionData));
         return {
             topic: 'licking-49744.new_question',
             partition: 0,
             message: {
-                value: JSON.stringify(data)
+                value: JSON.stringify(formattedQuestionData)
             }
         }
     });
